fbed575a949e4cb1b4ea52dd56ec69d3e5e7cd1f
eugenepsgit <blaze97197@gmail.com>
2024-04-29 23:54:47 +0700

    ready to deploy

---
+++ accordion.js
@@ -0,0 +1,63 @@
+const faqButton = document.querySelectorAll('.faq__row');
+
+faqButton.forEach((item) => {
+	item.addEventListener('click', () => {
+		item.parentNode.classList.toggle('faq__item--active');
+	});
+});
+
+function findElements(object, element) {
+	const instance = object;
+	instance.element = element;
+	instance.target = element.nextElementSibling;
+}
+
+function hideElement(object) {
+	const instance = object;
+	const { target } = instance;
+	target.style.height = null;
+	instance.isActive = false;
+}
+
+function showElement(object) {
+	const instance = object;
+	const { target, height } = instance;
+	target.style.height = `${height}px`;
+	instance.isActive = true;
+}
+
+function changeElementStatus(instance) {
+	if (instance.isActive) {
+		hideElement(instance);
+	} else {
+		showElement(instance);
+	}
+}
+
+function measureHeight(object) {
+	const instance = object;
+	instance.height = object.target.firstElementChild.clientHeight;
+}
+
+function subscribe(instance) {
+	instance.element.addEventListener('click', (event) => {
+		event.preventDefault();
+		changeElementStatus(instance);
+	});
+	window.addEventListener('resize', () => measureHeight(instance));
+}
+
+function accordion(element) {
+	const instance = {};
+
+	function init() {
+		findElements(instance, element);
+		measureHeight(instance);
+		subscribe(instance);
+	}
+
+	init();
+}
+
+const elements = [...document.querySelectorAll('.faq__row')];
+elements.forEach(accordion);
